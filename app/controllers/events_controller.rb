65b0e00d9bf608b8b760ce32912ae14f213281dc
mikey <mikey@hey.com>
2024-03-15 14:57:16 -0500

    editing

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,4 +4,21 @@ class EventsController < ApplicationController
     @events = Event.all
   end
 
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    event_params =
+      params.require(:event).
+        permit(:name, :description, :location, :price, :starts_at)
+    @event.update(event_params)
+    redirect_to @event
+  end
+
 end
