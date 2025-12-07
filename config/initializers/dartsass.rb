b9307ed114ec2a8ded05930d6cb515af5a727d94
Mikey Gough <mikey@hey.com>
2025-12-07 15:12:52 -0800

    Define load-path for dartsass

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -4,3 +4,5 @@ Rails.application.config.dartsass.builds = {
 }
 
 Rails.application.config.dartsass.build_options << " --style=compressed" if Rails.env.production?
+bootstrap_path = Bundler.rubygems.find_name('bootstrap').first.full_gem_path
+Rails.application.config.dartsass.build_options << " --load-path=#{bootstrap_path}/assets/stylesheets"
