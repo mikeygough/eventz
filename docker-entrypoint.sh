424206ac3d88c855e5edc60b0d9238dff5b4d2ba
Mikey Gough <mikey@hey.com>
2025-12-07 13:59:07 -0800

    Precompile assets for production in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 # Run database migrations
 bundle exec rails db:migrate
 
+# Precompile assets for production
+bundle exec rails assets:precompile
+
 # Then start the server
 exec bundle exec rails server -b 0.0.0.0
